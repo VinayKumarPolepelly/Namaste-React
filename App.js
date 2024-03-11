202a60d020129527d68fb97fe25d311cedd9d588
VinayKumarPolepelly <vinaykumarpolepelly30@gmail.com>
2024-03-11 18:11:12 +0530

    ep1

---
+++ App.js
@@ -0,0 +1,5 @@
+const parent=React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},
+React.createElement("h1",{id:"h1"},"iam h1")));
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
